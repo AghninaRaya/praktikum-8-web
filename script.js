752e62dff6a37a22549b74b34d177515433753de
AghninaRaya <arayaubaid@gmail.com>
2025-03-15 08:44:54 +0700

    praktikum 8 image

---
+++ script.js
@@ -0,0 +1,7 @@
+document.addEventListener("DOMContentLoaded", function () {
+    let carousel = new bootstrap.Carousel(document.getElementById("imageCarousel"), {
+        interval: 3000, // Gambar akan berganti setiap 3 detik
+        wrap: true
+    });
+  });
+  
